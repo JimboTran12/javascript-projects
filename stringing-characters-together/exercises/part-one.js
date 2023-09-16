c2b019284f2a819f709548000eec7f46511e5e17
Jimmy <hungtran2003ti@gmail.com>
2023-09-15 19:31:18 -0500

    String excercise completed

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,15 @@
-let num = 1001;
+let num = 120.01;
 
 //Returns 'undefined'.
 console.log(num.length);
-
+console.log(typeof num);
 //Use type conversion to print the length (number of digits) of an integer.
-
+num = String(num);
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+console.log(`Number : ${num}\nLength: ${num.length - 1}`)
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (num.indexOf('.') !== -1 ) { //Deximals
+    console.log(`Number : ${num}\nLength: ${num.length - 1}`);
+} else { // interger
+    console.log(`Number : ${num}\nLength: ${num.length}`);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,16 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+console.log(language.slice(0,1) + language.slice(4,5));
 //2. Without using slice(), use method chaining to accomplish the same thing.
-
+console.log(language.replace("ava", "").replace("cript",""));
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+console.log(`The abbreviation for 'JavaScript' is '${language[0] + language[4]}'`);
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
-
+console.log(language.slice(language.indexOf('J'), language.indexOf('S') + 1).replace("ava", ""))
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+console.log(notTitleCase[0].toUpperCase()+notTitleCase.slice(1,6)+notTitleCase[6].toLocaleUpperCase()+notTitleCase.slice(7,notTitleCase.length));
