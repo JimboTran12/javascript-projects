a49ae4d72576c63923a86df06ce263d71a609418
Jimmy <hungtran2003ti@gmail.com>
2023-10-30 15:16:39 -0500

    unit testin studio partially done

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,26 @@
 
 let launchcode = {
-  
+  organization: 'nonprofit',
+  executiveDirector: 'Jeff',
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOuput: function(num) {
+    if(num % 2 === 0) {
+        if (num % 3 === 0) {
+            if (num % 5 === 0) {
+                return 'LaunchCode Rocks!';
+            }
+            return 'LaunchCode!';
+        }
+        if (num % 5 === 0) {
+            return 'Launch Rocks!';
+        }
+        return 'Launch!';
+    }
+    if (num % 3 === 0) {
+        return 'Launch';
+    }
+  },
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,29 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
-  
+  // Write your unit tests here!\
+  test("value of 'organization' is 'nonprofit'", function() {
+    let output = launchcode.organization;
+    expect(output).toBe("nonprofit");
+  });
+
+  test("value of 'executiveDirector' is 'Jeff'", function() {
+    let output = launchcode.executiveDirector;
+    expect(output).toBe("Jeff");
+  });
+
+  test("value of 'percentageCoolEmployees' is 100", function() {
+    let output = launchcode.percentageCoolEmployees;
+    expect(output).toBe(100);
+  });
+
+  test("value of 'programsOffered' is ['Web Development', 'Data Analysis', 'Liftoff']", function() {
+    let output = launchcode.programsOffered;
+    let arr = ['Web Development', 'Data Analysis', 'Liftoff'];
+    expect(output).toEqual(arr);
+  });
+  test("When passed a number that is ONLY divisible by 2, launchOutput() returns 'Launch!'", function() {
+    let output = launchcode.launchOuput(4);
+    expect(output).toBe("'Launch!'");
+  })
 });
